80db2824945a113508717bdcb0f38d9f5d5303be
varnit-ta <varnitsingh65@gmail.com>
2025-08-16 04:20:51 +0530

    initial gRPC server

---
+++ server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/varnit-ta/gRPC-Practice/helloworld"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedHelloWorldServiceServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloWorldRequest) (*pb.HelloWorldResponse, error) {
+	return &pb.HelloWorldResponse{Message: "Hello! My name is Varnit!"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":4000")
+
+	if err != nil {
+		log.Fatalf("failed to listend on port 4000: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloWorldServiceServer(s, &Server{})
+
+	log.Printf("gRPC listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
